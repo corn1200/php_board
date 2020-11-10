a7a100fa52040638779ea4672a32cb99117a42e2
unknown <corn120033@gmail.com>
2020-11-10 17:25:23 +0900

    서버핑퐁 디버깅

--- php_board/view/loginpage.php
+++ php_board/view/loginpage.php
@@ -6,20 +6,12 @@
     <title>MainPage</title>
 </head>
 <body>
-    <?php
-        include '../controller/sessionHead.php';
-        
-        if($isLogin) {
-            header('Location: ./view/mainpage.php');
-        } else {
-    ?>
     <h1>로그인</h1>
     <form action="../controller/login.php" method="POST">
         <input type="text" name="id" placeholder="ID">
         <input type="password" name="password" placeholder="PASSWORD">
         <button>Login</button>
     </form>
-        <?php } ?>
     <a href="./signpage.php">회원가입</a>
 </body>
 </html>
